ce3bd436f64696bfc3b210c14dcd7122f5cb6804
okezieobi <obiedereokezie@gmail.com>
2019-07-16 16:20:17 +0100

    [Chore #166998425] Fix 8 for heroku postgres

--- api/src/migrations/migrate.sql
+++ api/src/migrations/migrate.sql
@@ -11,4 +11,5 @@ CREATE DATABASE wayfarer;
 \i api/src/tables/users.sql
 \i api/src/tables/buses.sql
 \i api/src/tables/trips.sql
-\i api/src/tables/bookings.sql 
+\i api/src/tables/bookings.sql
+\i api/src/seeders/users.sql
