fe65b026add6da27107cd999c4680c0e4afb9a94
okezieobi <obiedereokezie@gmail.com>
2019-07-17 10:27:09 +0100

    [Chore #167341500] Rearranged project files

--- api/src/migrations/migrate.sql
+++ api/src/migrations/migrate.sql
@@ -12,4 +12,3 @@ CREATE DATABASE wayfarer;
 \i api/src/tables/buses.sql
 \i api/src/tables/trips.sql
 \i api/src/tables/bookings.sql
-\i api/src/seeders/users.sql
--- api/src/tables/users.sql
+++ api/src/tables/users.sql
@@ -16,7 +16,7 @@ CREATE TABLE users
     username    varchar(128) NOT NULL UNIQUE,
     "password"  varchar(128) NOT NULL,
     "type"      varchar(128) DEFAULT 'Client',
-    is_admin    boolean      DEFAULT 'f',
+    is_admin    boolean      DEFAULT  false,
     create_date timestamptz  DEFAULT NOW(),
     modify_date timestamptz  DEFAULT NOW()
 );
